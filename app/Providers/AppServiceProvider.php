3c8de87128324dc33fd15fe0eed62ca457c3e86f
NikBent <nikolasbentus468@gmail.com>
2025-05-24 14:12:42 +0700

    admin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Str;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::composer('*', function ($view) {
+        if (Str::startsWith(request()->getHost(), 'admin.')) {
+            View::share('layout', 'layouts.admin'); // you can check this inside your views
+        }
+    });
     }
 }
